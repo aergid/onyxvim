dbd08c11a1c942a9e2b1e8c6e2371c16deca5758
aergid <develer@gmail.com>
2024-11-14 16:17:12 +0100

    Minor update
    Add annotations to some plugins;
    Fix mini with icons

--- modules/nixvim/ui/dressing-nvim.nix
+++ modules/nixvim/ui/dressing-nvim.nix
@@ -1,3 +1,5 @@
+# https://github.com/stevearc/dressing.nvim
+# Tune UI elements
 {
   lib,
   config,
--- modules/nixvim/ui/web-devicons.nix
+++ modules/nixvim/ui/web-devicons.nix
@@ -4,6 +4,7 @@
     ui.web-devicons.enable = lib.mkEnableOption "Enable web-devicons module";
   };
   config = lib.mkIf config.ui.web-devicons.enable {
+    plugins.mini.enable = true;
     plugins.mini.modules.icons.enable = true;
     plugins.mini.mockDevIcons = true;
   };
