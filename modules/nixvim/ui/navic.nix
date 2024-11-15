c6fd9c509f9fae02f6b660d87076083683f714f2
aergid <develer@gmail.com>
2024-11-15 20:37:09 +0100

    Enable navic and add it to lualine

---
+++ modules/nixvim/ui/navic.nix
@@ -0,0 +1,16 @@
+{
+  plugins.navic = {
+    enable = true;
+
+    settings = {
+      highliht = true;
+      separator = "  ";
+      lsp = {
+        auto_attach = true;
+        preference = [ "nixd" ];
+      };
+    };
+
+  };
+}
+
