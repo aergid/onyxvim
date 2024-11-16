f18f7ec76844868d592c59afccebb858eadc3b91
aergid <develer@gmail.com>
2024-11-16 11:28:12 +0100

    Revert icons back to webn-devicons

--- modules/nixvim/ui/web-devicons.nix
+++ modules/nixvim/ui/web-devicons.nix
@@ -4,8 +4,6 @@
     ui.web-devicons.enable = lib.mkEnableOption "Enable web-devicons module";
   };
   config = lib.mkIf config.ui.web-devicons.enable {
-    plugins.mini.enable = true;
-    plugins.mini.modules.icons.enable = true;
-    plugins.mini.mockDevIcons = true;
+    plugins.web-devicons.enable = true;
   };
 }
