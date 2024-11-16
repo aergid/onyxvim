2f5ba486a2f25b221fd24be1385499eae3ccdad2
aergid <develer@gmail.com>
2024-11-16 14:30:27 +0100

    Add code formatters

--- modules/nixvim/lsp/conform.nix
+++ modules/nixvim/lsp/conform.nix
@@ -1,25 +1,33 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 # Formatting plugin
 {
   options = {
     lsp.conform.enable = lib.mkEnableOption "Enable conform module";
   };
   config = lib.mkIf config.lsp.conform.enable {
+    extraPackages = with pkgs; [
+      black
+      prettierd
+      stylua
+      alejandra
+      rustfmt
+    ];
 
     plugins.conform-nvim = {
       enable = true;
       settings = {
         notify_on_error = true;
-        # default_format_opts = {
-        #   lsp_format = "fallback";
-        # };
-        # format_after_save = {
-        #   lsp_format = "fallback";
-        # };
+
         format_on_save = {
           timeoutMs = 500;
           lspFallback = true;
         };
+
         formatters_by_ft = {
           html = {
             __unkeyed-1 = "prettierd";
@@ -51,16 +59,15 @@
             __unkeyed-2 = "prettier";
             stop_after_first = true;
           };
-          java = [ "google-java-format" ];
-          python = [ "black" ];
-          lua = [ "stylua" ];
-          nix = [ "nixfmt" ];
+          python = ["black"];
+          lua = ["stylua"];
+          nix = ["alejandra"];
           markdown = {
             __unkeyed-1 = "prettierd";
             __unkeyed-2 = "prettier";
             stop_after_first = true;
           };
-          rust = [ "rustfmt" ];
+          rust = ["rustfmt"];
         };
       };
     };
