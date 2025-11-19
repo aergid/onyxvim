9086218ee688c2ea3e885a4526e3a4c82e8ed514
aergid <develer@gmail.com>
2025-11-19 11:24:58 +0100

    Improve sessions

--- lua/custom/plugins/persistence/persisted/init.lua
+++ lua/custom/plugins/persistence/persisted/init.lua
@@ -4,18 +4,26 @@ return {
     lazy = false,
     after = function(plugin)
       require("persisted").setup({
+        autostart = true,
         autoload = true, -- load session on startup
         on_autoload_no_session = function()
           vim.notify("No existing session to load.")
         end,
 
+        should_save = function()
+          local allowed = {
+            alpha = false
+          }
+          return allowed[vim.bo.filetype] or true
+        end,
+
         -- change session file name to match current working directory if it changes
         follow_cwd = false,
-
-        ignored_dirs = {
-          { "/home/" .. vim.fn.hostname(), exact = true },
-          { "/", exact = true },
+        allowed_dirs = {
+          "~/Develop",
+          "~/Notes",
         },
+
       })
       -- close nvim-tree before saving
       vim.api.nvim_create_autocmd({ "User" }, {
